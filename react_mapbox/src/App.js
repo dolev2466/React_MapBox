92576d7a9544f15371b4d46c6387fe6dac936a43
Dolev <dolev2466@gmail.com>
2021-01-25 20:17:32 +0200

    Add Marker by double click on the map
    but not save him yet

--- react_mapbox/src/App.js
+++ react_mapbox/src/App.js
@@ -1,15 +1,9 @@
 import React, { Fragment } from "react";
 import Map from "./components/Map.js";
-import NavBar from "./components/Navbar.js";
-import Sidebar from "./components/Sidebar.js";
 
 const App = () => {
   return (
-    <Fragment>
-      <Sidebar>
-        <Map />
-      </Sidebar>
-    </Fragment>
+        <Map/>
   );
 };
 
--- react_mapbox/src/components/Map.js
+++ react_mapbox/src/components/Map.js
@@ -7,6 +7,7 @@ import "./css/map.css";
 const Map = () => {
   const [places, setplaces] = useState([]);
   const [showPopup, setShowPopup] = useState({});
+  const [addMarker,setAddMarker]= useState(null);
   const [viewport, setViewport] = useState({
     width: "92vw",
     height: "94vh",
@@ -23,8 +24,12 @@ const Map = () => {
   }, []);
 
   const showAddMarkerPopup = (event)=>{
-    console.log(event);
-  }
+    const [longitude, latitude] = event.lngLat;
+    setAddMarker({
+      latitude,
+      longitude,
+    });
+  };
 
   return (
     <ReactMapGL
@@ -94,7 +99,48 @@ const Map = () => {
             </Popup>
           ) : null}
         </Fragment>
-      ))}
+      ))
+    }
+    {
+          addMarker ? (
+          <>
+          <Marker
+            latitude={addMarker.latitude}
+            longitude={addMarker.longitude}
+          >
+            <div>
+              <svg
+                className="marker red"
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }}
+                version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                <g>
+                  <g>
+                    <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                      c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                      c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </Marker>
+          <Popup
+            latitude={addMarker.latitude}
+            longitude={addMarker.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setAddMarker(null)}
+            anchor="top" >
+            <div className="popup">
+            <h1>Hii</h1>
+            </div>
+          </Popup>
+          </>
+        ) : null
+            }
     </ReactMapGL>
   );
 };
